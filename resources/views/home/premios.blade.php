e4bcba2d55038e9ff5ede7a45899460a94e05b5d
Cortes <8788870841diego@gmail.com>
2024-08-03 10:50:26 -0500

    Initial commit

---
+++ resources/views/home/premios.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.client')
+
+@section('content')
+<section>
+    <div class="">
+        <div class="">
+            
+        </div>
+    </div>
+</section>
+
+    Premios Increíbles:
+
+🏆 Primer Premio: ¡Un viaje todo incluido a un destino exótico!
+💎 Segundo Premio: ¡Una lujosa joya que deslumbrará a todos!
+🎮 Tercer Premio: ¡El último gadget tecnológico que todos desean!
+@endsection
